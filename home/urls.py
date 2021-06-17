cdd7d771083f5ec362e6dec5af1616ada777e895
nicovillal <nva010@alumnos.ucn.cl>
2021-06-16 22:04:57 -0400

    eliminar y crear paciente en planilla

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from home.views import *
-
-urlpatterns = [
-    path("home/", home)
-
-]
--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView, UpdateView, DeleteView
 from paciente.forms import PacienteForm
 from .models import paciente
+
+
 # Create your views here.
 
 
@@ -10,12 +12,26 @@ class Inicio(TemplateView):
     template_name = 'index.html'
 
 
-
-
+class ListadoPaciente(ListView):
+    model = paciente
+    template_name = 'paciente/listar_paciente.html'
+    context_object_name = 'pacientes'
+    queryset = paciente.objects.all()
 
 
 class CrearPaciente(CreateView):
     model = paciente
     form_class = PacienteForm
     template_name = 'paciente/crear_paciente.html'
+    success_url = reverse_lazy('paciente:listar_paciente')
+
+
+class EditarPaciente(UpdateView):
+    model = paciente
+    template_name = 'paciente/crear_paciente.html'
+    form_class = PacienteForm
+    success_url = reverse_lazy('paciente:listar_paciente')
+
+class EliminarPaciente(DeleteView):
+    model = paciente
     success_url = reverse_lazy('paciente:listar_paciente')
